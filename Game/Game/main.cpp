d4cf7ba033c8ee55ba47e91123225fddeb58b812
iAppleJack <iapplejack@ya.ru>
2015-03-14 20:54:19 +0300

    test

---
+++ Game/Game/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Game
+//
+//  Created by iAppleJack on 14/03/15.
+//  Copyright (c) 2015 iPrincessLuna. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
